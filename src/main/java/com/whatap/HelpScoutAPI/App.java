50ef2f37cfe3552fb06238068af0284a6ea1cef1
MinGu Lee <MinGu Lee>
2017-01-18 20:07:41 +0900

    Initial Commit

---
+++ src/main/java/com/whatap/HelpScoutAPI/App.java
@@ -0,0 +1,12 @@
+package com.whatap.HelpScoutAPI;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
